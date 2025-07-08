a6eec27962748b7239975a0213f2b100543f42e9
Alexandr Krivsic <o.kryvshych@gmail.com>
2025-07-08 03:07:31 +0200

    page module css

--- app/notes/error.tsx
+++ app/notes/error.tsx
@@ -13,25 +13,3 @@ const ErrorComponent = ({error}:Props) => {
 }
 
 export default ErrorComponent
-
-
-
-// 'use client';
-
-// type Props = {
-//   error: Error;
-// };
-
-// const Error = ({ error }: Props) => {
-//   return (
-//     <div>
-//       <p>Could not fetch the list of notes. {error.message}</p>
-//     </div>
-//   );
-// }
-
-// export default Error;
-
-// export default function ErrorPage({ error }: Props) {
-//   return <p>Could not fetch the list of notes. {error.message}</p>;
-// }
